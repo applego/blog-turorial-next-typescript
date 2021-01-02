e570420ec0806bef81ebf84ede156d8db6ec4427
applego <applegorillappa@gmail.com>
2021-01-03 00:06:11 +0900

    next/Headコンポーネントを追加し、titleを追記

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>NextJS Blog</title>
+      </Head>
       <main>Welcome</main>
     </div>
   );
