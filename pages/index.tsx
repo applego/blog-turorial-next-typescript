611ed6bb54e5dc13ac61f015d50777a810ddad9a
applego <applegorillappa@gmail.com>
2021-01-03 00:12:55 +0900

    favicon https://emojipedia.org/ を使用(copy image address)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,10 @@ export default function Home() {
     <div>
       <Head>
         <title>NextJS Blog</title>
+        <link
+          rel="icon"
+          href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/271/mountain_26f0-fe0f.png"
+        />
       </Head>
       <main>Welcome</main>
     </div>
